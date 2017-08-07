8fc34b10d562c562a302a41da4e49ff11af27e38
Luo Zhong <929269921@qq.com>
2017-08-07 13:53:49 +0800

    罗众

---
+++ src/service/i/DictionaryService.java
@@ -0,0 +1,15 @@
+package service.i;
+
+import entity.Dictionary;
+
+import java.util.List;
+
+/**
+ * Created by luozhong on 2017/8/6.
+ */
+public interface DictionaryService {
+    void addDictionary(Dictionary dictionary);
+    void deleteDictionary(Integer id);
+    List<Dictionary> showDictionary();
+    void updateDictionary(Dictionary dictionary);
+}
