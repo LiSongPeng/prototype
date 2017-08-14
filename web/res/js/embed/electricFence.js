382b522af1f92a824c205213ac980c0272ccee0a
JChang07 <guaishou07@yeah.net>
2017-08-14 15:22:36 +0800

    电子围栏页面基本完成(ver 0.1)

---
+++ web/res/js/embed/electricFence.js
@@ -0,0 +1,193 @@
+/**
+ * Created by Letg4 on 2017/8/14.
+ */
+var map= new BMap.Map("map-container");
+var point=new BMap.Point(117.041,39.108);
+const YYAPIH="http://yingyan.baidu.com/api/v3/fence/";
+map.centerAndZoom(point,11);
+map.enableScrollWheelZoom();
+map.addControl(new BMap.NavigationControl());
+var overlays=[];
+
+var styleOptions = {
+    strokeColor:"red",    //边线颜色。
+    fillColor:"red",            //填充颜色。当参数为空时，圆形将没有填充效果。
+    strokeWeight: 2,       //边线的宽度，以像素为单位。
+    strokeOpacity: 0.8,	   //边线透明度，取值范围0 - 1。
+    fillOpacity: 0.6,      //填充的透明度，取值范围0 - 1。
+    strokeStyle: 'solid' //边线的样式，solid或dashed。
+};
+var drawingManager = new BMapLib.DrawingManager(map,{
+    isOpen: false, //是否开启绘制模式
+    drawingToolOptions: {
+        anchor: BMAP_ANCHOR_TOP_RIGHT, //位置
+        offset: new BMap.Size(5, 5) //偏离值
+    },//是否显示工具栏
+    enableCalculate: false,
+    circleOptions: styleOptions, //圆的样式
+    polylineOptions: styleOptions, //线的样式
+    polygonOptions: styleOptions, //多边形的样式
+    rectangleOptions: styleOptions //矩形的样式
+});
+drawingManager.addEventListener('overlaycomplete',function (e) {
+    overlays.push(e.overlay);
+    console.log(e.overlay);
+    drawingManager.close();
+});
+var eleApp=angular.module('eleFence',[]);
+eleApp.controller('fenceCtrl',['$scope','$http','$rootScope',eleFenceCtrl]);
+function eleFenceCtrl($scope,$http,$rootScope) {
+    $scope.mapstatus="none";
+    $scope.mapstatusReset=function () {
+        if($scope.mapstatus=="new"){
+            $("#draw-edit").removeClass("active");
+            for(var i = 0; i < overlays.length; i++){
+                map.removeOverlay(overlays[i]);
+            }
+            overlays.length = 0;
+            drawingManager.close();
+            $(".btn-flipper").removeClass('rotated');
+        }else if($scope.mapstatus=="show"){
+            for(var i = 0; i < overlays.length; i++){
+                map.removeOverlay(overlays[i]);
+            }
+            overlays.length = 0;
+            $(".panel-body .btn-flipper.rotated").removeClass("rotated");
+            $(".collapse").slideUp('fast');
+        }
+        $scope.mapstatus="none";
+        return true;
+    };
+
+    $scope.fenceList=[
+        {id:1,name:'围栏1',type:'circle',monitor:'#allentity',overlay:{
+            center:{lng:117.041,lat:39.108},
+            radius:5000
+        }},
+        {id:3,name:'围栏2',type:'circle',monitor:'实体1',overlay:{
+            center:{lng:117.041,lat:39.108},
+            radius:2500.0
+        }},
+        {id:4,name:'围栏3',type:'polygon',monitor:'实体2',overlay:{
+            path:[{lng:117.1,lat:39.1},{lng:117.2,lat:39.1},{lng:117.2,lat:39.2},{lng:117.1,lat:39.2}]
+        }}
+    ];
+    $scope.drawFence=function (e) {
+//            if(overlays.length>0){
+//                alert("请先完成当前围栏编辑！");
+//                return;
+//            }
+        $scope.mapstatusReset();
+        $scope.mapstatus="new";
+        $("#draw-type-btn-group>.btn").removeClass("active");
+        switch ($(e.target).attr('draw-type')){
+            case "circle":
+                drawingManager.setDrawingMode(BMAP_DRAWING_CIRCLE);
+                break;
+            case "rectangle":
+                drawingManager.setDrawingMode(BMAP_DRAWING_RECTANGLE);
+                break;
+            case "polygon":
+                drawingManager.setDrawingMode(BMAP_DRAWING_POLYGON);
+                break;
+            default:
+                return;
+        }
+        $('.btn-flipper').addClass('rotated');
+        drawingManager.open();
+    };
+    $scope.drawCancel=function(e){
+        $scope.mapstatusReset();
+    };
+    $scope.drawEdit=function(e) {
+        if($(e.target).hasClass("active")){
+            overlays[0].disableEditing();
+            $(e.target).removeClass("active");
+        }else{
+            overlays[0].enableEditing();
+            $(e.target).addClass("active");
+        }
+        console.log(angular.toJson(overlays[0].getPath()));
+    };
+    $scope.drawSubmit=function(e) {
+        if($("#draw-edit").hasClass("active")){
+            overlays[0].disableEditing();
+            $("#draw-edit").removeClass("active");
+        }
+        var fence=overlays[0];
+        console.log(fence instanceof BMap.Circle);
+        console.log(fence.getCenter().lng);
+        console.log(fence.getCenter().lat);
+        console.log(fence.getRadius());
+        if (fence instanceof BMap.Circle){
+
+            alert("start!");
+
+        }
+    };
+    $scope.slctFence=function (e){
+        $scope.mapstatusReset();
+        if ($("#collapse"+$(e.target).attr('id')).css("display")!='none') {
+            return;
+        }
+        $("#collapse"+$(e.target).attr('id')).slideDown('fast');
+        $scope.mapstatus="show";
+        var fenceid=$(e.target).attr('id');
+        for(var i=0;i<$scope.fenceList.length;i++){
+            if($scope.fenceList[i].id==fenceid){
+                if($scope.fenceList[i].type=='circle'){
+                    var ovlay=new BMap.Circle($scope.fenceList[i].overlay.center,
+                        $scope.fenceList[i].overlay.radius,
+                        styleOptions);
+                    overlays.push(ovlay);
+                    map.addOverlay(ovlay);
+                }else if($scope.fenceList[i].type=='polygon'){
+                    var path=$scope.fenceList[i].overlay.path;
+                    var ovlay=new BMap.Polygon(path,styleOptions);
+                    overlays.push(ovlay);
+                    map.addOverlay(ovlay);
+                }
+            }
+        }
+    }
+    $scope.editFence=function (e) {
+        overlays[0].enableEditing();
+        $(e.target).parent().parent().addClass("rotated");
+    }
+    $scope.cancelEditFence= function (e) {
+        for(var i = 0; i < overlays.length; i++){
+            map.removeOverlay(overlays[i]);
+        }
+        overlays.length = 0;
+        var id=$(e.target).parents(".panel-body").attr("id").substring(8);
+        $scope.fenceList.forEach(function (value, index, arr) {
+            if(value.id==id){
+                if(value.type=='circle'){
+                    var ovlay=new BMap.Circle(value.overlay.center,
+                        value.overlay.radius,
+                        styleOptions);
+                }else if(value.type=='polygon'){
+                    var ovlay=new BMap.Polygon(value.overlay.path,styleOptions);
+                }
+                overlays.push(ovlay);
+                map.addOverlay(ovlay);
+            }
+        })
+        $(e.target).parent().parent().removeClass("rotated");
+    }
+    $scope.commitEditFence=function (e) {
+        overlays[0].disableEditing();
+        var id=$(e.target).parents(".panel-body").attr("id").substring(8);
+        for(var i=0;i<$scope.fenceList.length;i++){
+            if($scope.fenceList[i].id==id) {
+                if ($scope.fenceList[i].type == 'circle') {
+                    $scope.fenceList[i].overlay.center = overlays[0].getCenter();
+                    $scope.fenceList[i].overlay.radius = overlays[0].getRadius();
+                } else if ($scope.fenceList[i].type == 'polygon') {
+                    $scope.fenceList[i].setPath($scope.fenceList[i].overlay.path);
+                }
+            }
+        }
+        $(e.target).parent().parent().removeClass("rotated");
+    }
+}
