77ae336d620b88f99457f70cf58c8802d1d94d5d
poplarnotree <546837635@qq.com>
2017-08-31 10:29:30 +0800

    修改数据库

--- updateSql.sql
+++ updateSql.sql
@@ -1,5 +1,9 @@
+-- 2017.8.31
 -- 修改person表中u_id为id
 ALTER TABLE person CHANGE COLUMN u_id id VARCHAR(36);
 -- 修改person表中，passWord字段名及长度
 ALTER TABLE person CHANGE COLUMN passWord password VARCHAR(32) NOT NULL ;
---
+-- 修改role表中r_id
+ALTER TABLE role CHANGE COLUMN r_id id VARCHAR(36);
+-- 修改company表中c_id
+ALTER TABLE company CHANGE COLUMN c_id id VARCHAR (36);
